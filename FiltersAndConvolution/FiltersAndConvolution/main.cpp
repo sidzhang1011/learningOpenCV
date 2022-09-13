0193540ad944725af60e39594e6bb391f020b66f
Sid Zhang <zsg@outlook.com>
2022-09-13 18:40:54 +0800

    create filter and convolution project

---
+++ FiltersAndConvolution/FiltersAndConvolution/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FiltersAndConvolution
+//
+//  Created by Sid Zhang on 2022/9/13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
